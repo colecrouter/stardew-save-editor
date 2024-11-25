410daa6bee3387bd222950f683ac0ac903c72fa8
colecrouter <colecrouter1@gmail.com>
2024-11-25 10:39:05 -0600

    Improved farmhands list styling

--- src/routes/(edit)/farmhands/+page.svelte
+++ src/routes/(edit)/farmhands/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { saveManager } from "$lib/save.svelte";
     import UiContainer from "$lib/ui/UIContainer.svelte";
+    import UiContainerSmall from "$lib/ui/UIContainerSmall.svelte";
     import Profiles from "./Profiles.svelte";
 
     const save = saveManager.save;
@@ -8,8 +9,26 @@
 </script>
 
 <UiContainer>
-    <Profiles farmers={save.players} />
+    <h2>Drag and drop to reorder your farmhands</h2>
+
+    <div class="wrapper">
+        <UiContainerSmall>
+            <Profiles farmers={save.players} />
+        </UiContainerSmall>
+    </div>
 </UiContainer>
 
 <style>
+    h2 {
+        font-size: large;
+        text-align: center;
+        margin-bottom: 1em;
+        padding-top: 0;
+    }
+
+    .wrapper {
+        display: flex;
+        width: 100%;
+        justify-content: center;
+    }
 </style>
--- src/routes/(edit)/farmhands/Profiles.svelte
+++ src/routes/(edit)/farmhands/Profiles.svelte
@@ -9,7 +9,7 @@
 
 <script lang="ts">
     import type { Farmer } from "$lib/proxies/Farmer";
-    import CharacterPreview from "../appearance/CharacterPreview.svelte";
+    import Character from "../appearance/Character.svelte";
 
     interface Props {
         farmers: Farmer[];
@@ -32,39 +32,49 @@
     {@const farmName = farmer.raw.farmName}
 
     <div class="wrapper">
-        <div class="inner">
-            <div class="content">
-                <div class="left">
-                    <div class="number">
-                        {i + 1}.
-                    </div>
-                    <CharacterPreview player={farmer} />
-                    <div class="details">
-                        <h2 class="name">
-                            {farmer.name}
-                        </h2>
-                        <div class="date">
-                            Day {day} of {season}, Year {year}
+        <div class="outer">
+            <div class="inner">
+                <div class="content">
+                    <div class="left">
+                        <div class="number">
+                            {i + 1}.
+                        </div>
+                        <div class="character">
+                            <Character player={farmer} />
+                        </div>
+                        <div class="details">
+                            <h2 class="name">
+                                {farmer.name}
+                            </h2>
+                            <div class="date">
+                                Day {day} of {season}, Year {year}
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="right">
-                    <div class="farm-name">
-                        {farmName} Farm
-                    </div>
-                    <div class="stats">
-                        <div class="stat">
-                            <label for="money-earned" hidden>Money Earned</label
-                            >
-                            <span id="money-earned" aria-label="Money Earned">
-                                🪙 {moneyEarned}
-                            </span>
+                    <div class="right">
+                        <div class="farm-name">
+                            {farmName} Farm
                         </div>
-                        <div class="stat">
-                            <label for="time-played" hidden>Time Played</label>
-                            <span id="time-played" aria-label="Time Played">
-                                🕒 {hoursPlayed}:{minutesPlayed}
-                            </span>
+                        <div class="stats">
+                            <div class="stat">
+                                <label for="money-earned" hidden
+                                    >Money Earned</label
+                                >
+                                <span
+                                    id="money-earned"
+                                    aria-label="Money Earned"
+                                >
+                                    💵 {moneyEarned}
+                                </span>
+                            </div>
+                            <div class="stat">
+                                <label for="time-played" hidden
+                                    >Time Played</label
+                                >
+                                <span id="time-played" aria-label="Time Played">
+                                    🕒 {hoursPlayed}:{minutesPlayed}
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -74,7 +84,24 @@
 {/each}
 
 <style>
+    .wrapper {
+        border: 1px solid #6f3b31;
+        border-radius: 2px;
+    }
+
+    .outer {
+        border: 2px solid #fff;
+    }
+
+    .inner {
+        border: 2px solid #9c613b;
+    }
+
     .content {
+        padding: 1rem;
+        margin: -2px;
+        border: 2px solid #9c613b;
+        border-radius: 10px;
         display: flex;
         justify-content: space-between;
     }
@@ -88,6 +115,11 @@
         font-size: large;
         font-weight: bold;
         margin-right: 1em;
+    }
+
+    .character {
+        max-height: 60px;
+        transform: translateY(-10px);
     }
 
     .details {
@@ -111,5 +143,9 @@
 
     .stat {
         display: inline-block;
+    }
+
+    .farm-name {
+        font-style: italic;
     }
 </style>

7e4bf7d6d913784ee0e79767f6000b616b2d8be5
colecrouter <colecrouter1@gmail.com>
2025-01-10 08:34:11 -0600

    Removed sentry from server dev build

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -14,7 +14,7 @@ dev ||
     });
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
+export const handle = dev ? undefined : sequence(sentryHandle());
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
-export const handleError = handleErrorWithSentry();
+export const handleError = dev ? undefined : handleErrorWithSentry();

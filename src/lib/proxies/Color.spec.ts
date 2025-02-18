e3b727d4ec3d7da8eabef40196ead7f5f2fedc7d
colecrouter <colecrouter1@gmail.com>
2025-02-18 11:24:22 -0600

    Moved tests/proxies/Color.test.ts to src/lib/proxies/Color.spec.ts

--- src/lib/proxies/Color.spec.ts
+++ src/lib/proxies/Color.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from "vitest";
-import { Color } from "../../src/lib/proxies/Color";
+import { Color } from "./Color";
 
 describe("Color", () => {
     it("should return correct PackedValue for yellow", () => {
